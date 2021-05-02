1d5334c574f834c83a3d7ee6640423f6e9989254
Taeshinobu <kobe.laprade@gmail.com>
2021-05-02 18:50:00 -0400

    Intro cutscene

---
+++ Assets/Scripts/IntroCutscene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroCutscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadLevel", 20f);
+    }
+    void LoadLevel()
+    {
+        SceneManager.LoadScene("AncientEgyptScene");
+    }
+
+}
